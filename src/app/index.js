5f0ebed52199018233d7050cf2db86aa8d3877f1
Marinovn <64974112+Marinovn@users.noreply.github.com>
2020-08-20 18:56:07 -0300

    Add files via upload

---
+++ src/app/index.js
@@ -0,0 +1,36 @@
+const { Weather } = require( './Weather');
+const { UI } = require('./Ui');
+const { Store } = require ('./Store');
+
+const store = new Store();
+const { city, countryCode } = store.getLocationData();
+
+const weather = new Weather(city, countryCode);
+const ui = new UI();
+
+
+
+require('./index.css');
+
+
+async function fetchWeather() {
+    const data = await weather.getWeather();
+    console.log(data);
+    ui.render(data);
+}
+
+document.addEventListener('DOMContentLoaded', fetchWeather);
+
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const countryCode = document.getElementById('countryCode').value;
+    
+    weather.changeLocation(city, countryCode);
+
+    store.setLocationData(city, countryCode);
+
+    fetchWeather();
+    
+    e.preventDefault();
+
+});
